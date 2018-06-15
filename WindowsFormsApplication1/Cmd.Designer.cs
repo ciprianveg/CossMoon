dca19e41c2563f62e6bbf091a467f976fa509a9a
ciprianveg <40246315+ciprianveg@users.noreply.github.com>
2018-06-15 13:25:42 +0300

    Add files via upload

---
+++ WindowsFormsApplication1/Cmd.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Cmd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Cmd));
+            this.outputField = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.inputField = new System.Windows.Forms.TextBox();
+            this.buyButton = new System.Windows.Forms.Button();
+            this.sellButton = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.balanceButton = new System.Windows.Forms.Button();
+            this.pairDepthButton = new System.Windows.Forms.Button();
+            this.orderHistoryButton = new System.Windows.Forms.Button();
+            this.checkMarketParirsButton = new System.Windows.Forms.Button();
+            this.marketTickerDataButton = new System.Windows.Forms.Button();
+            this.allTickerDataButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.helpButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // outputField
+            // 
+            resources.ApplyResources(this.outputField, "outputField");
+            this.outputField.Name = "outputField";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // inputField
+            // 
+            resources.ApplyResources(this.inputField, "inputField");
+            this.inputField.Name = "inputField";
+            // 
+            // buyButton
+            // 
+            this.buyButton.BackColor = System.Drawing.Color.YellowGreen;
+            resources.ApplyResources(this.buyButton, "buyButton");
+            this.buyButton.Name = "buyButton";
+            this.buyButton.UseVisualStyleBackColor = false;
+            this.buyButton.Click += new System.EventHandler(this.buy_Click);
+            // 
+            // sellButton
+            // 
+            this.sellButton.BackColor = System.Drawing.Color.Tomato;
+            resources.ApplyResources(this.sellButton, "sellButton");
+            this.sellButton.Name = "sellButton";
+            this.sellButton.UseVisualStyleBackColor = false;
+            this.sellButton.Click += new System.EventHandler(this.sellButton_Click);
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(this.checkBox1, "checkBox1");
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // balanceButton
+            // 
+            resources.ApplyResources(this.balanceButton, "balanceButton");
+            this.balanceButton.Name = "balanceButton";
+            this.balanceButton.UseVisualStyleBackColor = true;
+            this.balanceButton.Click += new System.EventHandler(this.balance_Click);
+            // 
+            // pairDepthButton
+            // 
+            resources.ApplyResources(this.pairDepthButton, "pairDepthButton");
+            this.pairDepthButton.Name = "pairDepthButton";
+            this.pairDepthButton.UseVisualStyleBackColor = true;
+            this.pairDepthButton.Click += new System.EventHandler(this.pairDepthButton_Click);
+            // 
+            // orderHistoryButton
+            // 
+            resources.ApplyResources(this.orderHistoryButton, "orderHistoryButton");
+            this.orderHistoryButton.Name = "orderHistoryButton";
+            this.orderHistoryButton.UseVisualStyleBackColor = true;
+            this.orderHistoryButton.Click += new System.EventHandler(this.orderHistoryButton_Click);
+            // 
+            // checkMarketParirsButton
+            // 
+            resources.ApplyResources(this.checkMarketParirsButton, "checkMarketParirsButton");
+            this.checkMarketParirsButton.Name = "checkMarketParirsButton";
+            this.checkMarketParirsButton.UseVisualStyleBackColor = true;
+            this.checkMarketParirsButton.Click += new System.EventHandler(this.checkMarketParirsButton_Click);
+            // 
+            // marketTickerDataButton
+            // 
+            resources.ApplyResources(this.marketTickerDataButton, "marketTickerDataButton");
+            this.marketTickerDataButton.Name = "marketTickerDataButton";
+            this.marketTickerDataButton.UseVisualStyleBackColor = true;
+            this.marketTickerDataButton.Click += new System.EventHandler(this.marketTickerDataButton_Click);
+            // 
+            // allTickerDataButton
+            // 
+            resources.ApplyResources(this.allTickerDataButton, "allTickerDataButton");
+            this.allTickerDataButton.Name = "allTickerDataButton";
+            this.allTickerDataButton.UseVisualStyleBackColor = true;
+            this.allTickerDataButton.Click += new System.EventHandler(this.allTickerDataButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            resources.ApplyResources(this.cancelButton, "cancelButton");
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.UseVisualStyleBackColor = false;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // helpButton
+            // 
+            resources.ApplyResources(this.helpButton, "helpButton");
+            this.helpButton.Name = "helpButton";
+            this.helpButton.UseVisualStyleBackColor = true;
+            this.helpButton.Click += new System.EventHandler(this.helpButton_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // Cmd
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.helpButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.allTickerDataButton);
+            this.Controls.Add(this.marketTickerDataButton);
+            this.Controls.Add(this.checkMarketParirsButton);
+            this.Controls.Add(this.orderHistoryButton);
+            this.Controls.Add(this.pairDepthButton);
+            this.Controls.Add(this.balanceButton);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.sellButton);
+            this.Controls.Add(this.buyButton);
+            this.Controls.Add(this.inputField);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.outputField);
+            this.Name = "Cmd";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox outputField;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox inputField;
+        private System.Windows.Forms.Button buyButton;
+        private System.Windows.Forms.Button sellButton;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button balanceButton;
+        private System.Windows.Forms.Button pairDepthButton;
+        private System.Windows.Forms.Button orderHistoryButton;
+        private System.Windows.Forms.Button checkMarketParirsButton;
+        private System.Windows.Forms.Button marketTickerDataButton;
+        private System.Windows.Forms.Button allTickerDataButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button helpButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Cmd.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Cmd : Form
+    {
+        public Cmd()
+        {
+            InitializeComponent();
+            Load();
+            this.KeyPreview = true;
+        }
+
+        System.Diagnostics.Process p;
+        StreamWriter inputStream;
+        StreamReader outputStream;
+        StreamReader errorStream;
+        byte[] readOutputBuffer = new byte[4096];
+        byte[] readErrorBuffer = new byte[4096];
+        Font outputFont = new Font("Consolas", 10);
+        Font errorFont = new Font("Consolas", 10, FontStyle.Bold);
+        Color outputColor = Color.LightGray;
+        Color errorColor = Color.Red;
+        
+        private void Load()
+        {
+            inputField.Select();
+           // inputField.BackColor = Color.Black;
+            //inputField.SelectionColor = outputColor;
+            //inputField.SelectionFont = outputFont;
+            outputField.BackColor = Color.Black;
+
+            ProcessStartInfo ps = new ProcessStartInfo("cmd.exe");
+            ps.RedirectStandardInput = true;
+            ps.RedirectStandardOutput = true;
+            ps.RedirectStandardError = true;
+            ps.UseShellExecute = false;
+            ps.CreateNoWindow = true;
+            ps.StandardErrorEncoding = Encoding.ASCII;
+            ps.StandardOutputEncoding = Encoding.ASCII;
+            try
+            {
+                p = Process.Start(ps);
+                inputStream = p.StandardInput;
+                outputStream = p.StandardOutput;
+                errorStream = p.StandardError;
+                DoReadOutput();
+                DoReadError();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message); // Just in case there's a problem on your end. :-)
+            }
+        }
+
+        void DoReadOutput()
+        {
+            outputStream.BaseStream.BeginRead(readOutputBuffer, 0, readOutputBuffer.Length, new AsyncCallback(OnReadOutputCompleted), null);
+        }
+
+        void OnReadOutputCompleted(IAsyncResult result)
+        {
+            int cbRead = outputStream.BaseStream.EndRead(result);
+            ProcessOutput(readOutputBuffer, cbRead);
+            DoReadOutput();
+        }
+
+        void DoReadError()
+        {
+            errorStream.BaseStream.BeginRead(readErrorBuffer, 0, readErrorBuffer.Length, new AsyncCallback(OnReadErrorCompleted), null);
+        }
+
+        void OnReadErrorCompleted(IAsyncResult result)
+        {
+            int cbRead = errorStream.BaseStream.EndRead(result);
+            ProcessError(readErrorBuffer, cbRead);
+            DoReadError();
+        }
+
+        private void ProcessOutput(byte[] buffer, int cbRead)
+        {
+            string text = p.StartInfo.StandardOutputEncoding.GetString(buffer, 0, cbRead);
+            this.Invoke((Action)delegate
+            {
+                this.outputField.SelectionFont = outputFont;
+                this.outputField.SelectionColor = outputColor;
+                this.outputField.AppendText(text);
+                this.outputField.SelectionStart = this.outputField.Text.Length;
+                this.outputField.ScrollToCaret();
+            });
+        }
+
+        private void ProcessError(byte[] buffer, int cbRead)
+        {
+            string text = p.StartInfo.StandardErrorEncoding.GetString(buffer, 0, cbRead);
+            this.Invoke((Action)delegate
+            {
+                this.outputField.SelectionFont = errorFont;
+                this.outputField.SelectionColor = errorColor;
+                this.outputField.AppendText(text);
+                this.outputField.SelectionStart = this.outputField.Text.Length;
+                this.outputField.ScrollToCaret();
+            });
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Send();
+            }
+            base.OnKeyDown(e);
+        }
+
+        void Send()
+        {
+            string text = this.inputField.Text+"\n";
+            this.inputField.Text = "";
+            byte[] bytes = Encoding.ASCII.GetBytes(text);
+            if (bytes != null)
+            {
+                inputStream.BaseStream.Write(bytes, 0, bytes.Length);
+                inputStream.Flush();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Send();
+        }
+
+        private void buy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sellButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void balance_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void helpButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pairDepthButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void orderHistoryButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkMarketParirsButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void marketTickerDataButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void allTickerDataButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
